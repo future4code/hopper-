e63dc8861eae39c6d68902dbd762a7dc0dce5e00
Fernanda Pires <nanbnis@gmail.com>
2022-02-08 13:27:05 -0300

    PR1 da semana 2

---
+++ modulo2/PR1/variaveis/template_javascript/index.js
@@ -0,0 +1,54 @@
+// 1. Será impresso o seguinte: 10, 5
+// 2. Será impresso o seguinte: 10, 10, 10.
+// 3. horasDia e salarioDia
+
+
+
+//let arNome = null
+//let arIdade = null 
+
+//console.log(typeof arNome, arIdade)
+
+// pois o comando null indica uma váriavel vazia. 
+
+/*let arNome = prompt ("Qual seu nome?")
+let arIdade = prompt ("Qual sua idade?")
+
+console.log(typeof arNome, typeof arIdade)
+
+console.log("Olá" , arNome ,"você tem" , arIdade , "anos.")
+
+let p1 = prompt("sua blusa é amarela?")
+let p2 = prompt("você está descalçx?")
+let p3 = prompt("você está sentadx?")
+
+console.log("sua blusa é amarela? -", p1, "você está descalçx? -" , p2, "você está sentadx? -", p3)
+
+
+let a = 10
+let b = 25
+let c = 0
+
+
+c = a
+a = b
+b = c
+
+console.log("o novo valor de a é", a)
+console.log("o novo valor de b é", b)
+
+// EXERCICIO OPCIONAL
+
+let n1 = prompt("insira um número")
+let n2 = prompt("insira mais um número")
+
+n3 = Number(n1)
+n4 = Number(n2)
+
+nS = n3+n4
+nM = n3*n4
+
+console.log("O primeiro numero somado com o segundo dá", nS)
+console.log("o primeiro número multiplicado com o segundo núnero dá" , nM)*/
+
+
