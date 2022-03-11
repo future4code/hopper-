69803d601200470ea674de1f28bf60a973c0aad7
Fernanda Pires <nanbnis@gmail.com>
2022-03-11 18:36:54 -0300

    Projeto Blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,31 @@
  * 
  * 
  */
+
+const primeiraCarta = comprarCarta()
+const segundaCarta = comprarCarta()
+const terceiraCarta = comprarCarta()
+const quartaCarta = comprarCarta()
+
+
+console.log("Boas vindas ao jogo Blackjack!")
+
+const mensagemIncioDeJogo = confirm("Deseja iniciar o jogo?")
+   if (mensagemIncioDeJogo === false) {
+      console.log("O jogo acabou.")    
+   } else {
+      let cartasUsuarios = (primeiraCarta.valor + segundaCarta.valor)
+      let cartasComputador = (terceiraCarta.valor + quartaCarta.valor)
+
+      console.log(`Usuário => cartas: ${primeiraCarta.texto}, ${segundaCarta.texto} | pontuação: ${cartasUsuarios}`)
+      console.log(`Computador => cartas: ${terceiraCarta.texto}, ${quartaCarta.texto} | pontuação: ${cartasComputador}`)
+      if (cartasComputador > cartasUsuarios) {
+         console.log("O computador ganhou!")
+      }
+      if (cartasComputador < cartasUsuarios) {
+         console.log("O usuário ganhou!")
+      } 
+      if (cartasComputador === cartasUsuarios) {
+         console.log("Empate!")
+      }
+   }
